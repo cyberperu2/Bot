1fb3a0c368de5383e98ffa1a87990c0d3b6894d2
Paper890 <140361640+Paper890@users.noreply.github.com>
2023-09-09 23:21:47 +0800

    Update Telebot.py

--- Telebot.py
+++ Telebot.py
@@ -1,7 +1,7 @@
 const fetch = require('node-fetch');
 const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot('YOUR_BOT_TOKEN', {polling: true});
+const bot = new TelegramBot('6338068936:AAHiZflXdNWM6vrWgu474SXhzxcKZ_ITDv0', {polling: true});
 
 bot.onText(/\/gettext/, (msg) => {
  const chatId = msg.chat.id;
