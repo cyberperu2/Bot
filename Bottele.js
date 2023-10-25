89d50d839d49d537299a3e582afc12e5e6504048
cyberperu2 <148935264+cyberperu2@users.noreply.github.com>
2023-10-25 00:38:55 -0500

    Update Bottele.js

--- Bottele.js
+++ Bottele.js
@@ -1,11 +1,11 @@
 const fetch = require('node-fetch');
 const TelegramBot = require('node-telegram-bot-api');
 
-const bot = new TelegramBot('6338068936:AAHiZflXdNWM6vrWgu474SXhzxcKZ_ITDv0', {polling: true});
+const bot = new TelegramBot('6411622214:AAFnHMSf-D4zmw-hwq4hveU3MQv-9qgxFcU', {polling: true});
 
 bot.onText(/\/gettext/, (msg) => {
  const chatId = msg.chat.id;
- const githubPath = 'https://raw.githubusercontent.com/Paper890/izin/main/IP';
+ const githubPath = 'https://raw.githubusercontent.com/cyberperu2/izin/main/IP';
  fetch(`https://raw.githubusercontent.com/${githubPath}`)
    .then(response => response.text())
    .then(text => {
